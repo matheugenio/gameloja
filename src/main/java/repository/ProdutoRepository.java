bdee3774114471f78a9d02d94367df27139376a5
Matheus Eugenio <math.eugenioti@gmail.com>
2024-07-10 11:47:47 -0300

    feat: Criação das classes repository de Categoria e Produto

---
+++ src/main/java/repository/ProdutoRepository.java
@@ -0,0 +1,14 @@
+package repository;
+
+import model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    public List<Produto> findAllByPlataformaoContainingIgnoreCase(@Param("plataforma") String plataforma);
+
+}
